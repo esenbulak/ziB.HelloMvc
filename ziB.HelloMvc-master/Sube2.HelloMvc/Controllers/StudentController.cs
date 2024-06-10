9b7a5c4b898ff8298846ad6de16abbb7fe9dd5fe
Esen <esennbulak@gmail.com>
2024-06-10 03:27:14 +0300

    ziB.HelloMvc

---
+++ ziB.HelloMvc-master/Sube2.HelloMvc/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sube2.HelloMvc.Models;
+using System.Net;
+
+namespace Sube2.HelloMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var ctx = new OkulDbContext())
+            {
+                var lst = ctx.Ogrenciler.ToList();
+                return View(lst);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Ogrenci ogr)
+        {
+            if (ogr != null)
+            {
+                ;
+                using (var ctx = new OkulDbContext())
+                {
+                    ctx.Ogrenciler.Add(ogr);
+                    ctx.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditStudent(int id)
+        {
+            using (var ctx = new OkulDbContext())
+            {
+                var ogr = ctx.Ogrenciler.Find(id);
+                return View(ogr);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent(Ogrenci ogr)
+        {
+            if (ogr != null)
+            {
+                using (var ctx = new OkulDbContext())
+                {
+                    ctx.Entry(ogr).State = EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            using (var ctx = new OkulDbContext())
+            {
+                ctx.Ogrenciler.Remove(ctx.Ogrenciler.Find(id));
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult AssignCourse(int? id)
+        {
+            using (var ctx = new OkulDbContext()) {
+                Ogrenci student = ctx.Ogrenciler.Include(s => s.Dersler).FirstOrDefault(s => s.Ogrenciid == id);
+                ViewBag.CourseId = new SelectList(ctx.Dersler, "Dersid", "Dersad");
+                return View(student);
+            }
+ 
+        }
+        public ActionResult AssignCourse(int id, int courseId)
+        {
+            using (var ctx = new OkulDbContext()) {
+                Ogrenci student = ctx.Ogrenciler.Include(s => s.Dersler).FirstOrDefault(s => s.Ogrenciid == id);
+
+                Ders course = ctx.Dersler.Find(courseId);
+
+
+                student.Dersler.Add(course);
+                ctx.SaveChanges();
+
+                return RedirectToAction("Details", new { id = student.Ogrenciid });
+            }
+               
+        }
+        public ActionResult Courses(int? id)
+        {
+            using (var ctx = new OkulDbContext()) {
+                Ogrenci student = ctx.Ogrenciler.Include(s => s.Dersler).FirstOrDefault(s => s.Ogrenciid == id);
+
+                return View(student);
+            }
+                
+        }
+    }
+}
---
+++ ziB.HelloMvc-master/Sube2.HelloMvc/Migrations/20240609140051_AddCourseEntity.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sube2.HelloMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dersler",
+                columns: table => new
+                {
+                    Dersid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dersad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kredi = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dersler", x => x.Dersid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DersOgrenci",
+                columns: table => new
+                {
+                    DerslerDersid = table.Column<int>(type: "int", nullable: false),
+                    OgrencilerOgrenciid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DersOgrenci", x => new { x.DerslerDersid, x.OgrencilerOgrenciid });
+                    table.ForeignKey(
+                        name: "FK_DersOgrenci_Dersler_DerslerDersid",
+                        column: x => x.DerslerDersid,
+                        principalTable: "Dersler",
+                        principalColumn: "Dersid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DersOgrenci_tblOgrenciler_OgrencilerOgrenciid",
+                        column: x => x.OgrencilerOgrenciid,
+                        principalTable: "tblOgrenciler",
+                        principalColumn: "Ogrenciid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DersOgrenci_OgrencilerOgrenciid",
+                table: "DersOgrenci",
+                column: "OgrencilerOgrenciid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DersOgrenci");
+
+            migrationBuilder.DropTable(
+                name: "Dersler");
+        }
+    }
+}
